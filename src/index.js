f14714e600aa742c513b0982ea6848caf1f2a2fd
Vi Tran <contact.vitran@gmail.com>
2021-11-28 18:11:31 +1100

    All core deliverables complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const beerName = document.querySelector('div.beer-details h2');
 const beerImage = document.querySelector('div.beer-details img');
+const beerDescForm = document.querySelector('form.description');
 const beerDescription = document.querySelector('form.description textarea');
-const updateDescButton = document.querySelector('form.description button');
+const reviewForm = document.querySelector('form.review-form');
 const reviewInput = document.querySelector('form.review-form textarea');
 //const reviewSubmit = document.querySelector("form.review-form input[type='submit']");
 const reviewsList = document.querySelector('ul.reviews');
+let beerToDisplay;
 
 const getData = async (url) => {
     const response = await fetch(url);
@@ -24,9 +26,34 @@ const renderBeerDetails = function (beer) {
     });
 }
 
+const changeBeerDesciption = async function (id, text) {
+    let patchObj = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          "description": text,
+        })
+    };
+    await fetch(`http://localhost:3000/beers/${id}`, patchObj);
+}    
+
 document.addEventListener('DOMContentLoaded', async () => {
-    const beerToDisplay = await getData('http://localhost:3000/beers/1');
-    console.log(beerToDisplay);
+    beerToDisplay = await getData('http://localhost:3000/beers/1');
     renderBeerDetails(beerToDisplay);
 })
 
+beerDescForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    await changeBeerDesciption(beerToDisplay.id, beerDescription.value);
+})
+
+reviewForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const reviewItem = document.createElement('li');
+    reviewItem.innerHTML = reviewInput.value;
+    reviewsList.appendChild(reviewItem);
+    reviewInput.value = "";
+})
